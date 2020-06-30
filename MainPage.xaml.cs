99899f6f9662aa7a1631d5fd70804a5850c0ffeb
Tupiet <Tupiet@users.noreply.github.com>
2020-06-30 17:29:45 +0200

    Several Updates
    # NEW FEATURES:
    Decompress.
    Progress Bar.
    Dark mode.
    Help window (beta, isn't working)
    
    # BUG FIXED:
    Now, when you don't select a file, the app doesn't crash.
    
    # COMING...
    In new versions, the "help window" will be modified and will be working correctly.
    
    # COMMENTS:
    This update is SO big, so when the help window will be working, the app will launch like version 2.0.0

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
@@ -7,10 +8,12 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Foundation.Diagnostics;
 using Windows.Storage;
 using Windows.Storage.AccessCache;
 using Windows.Storage.Pickers;
 using Windows.UI.Popups;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -18,6 +21,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
 
@@ -31,14 +35,21 @@ namespace NewZip
         public MainPage()
         {
             this.InitializeComponent();
+
         }
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
             string wishedZipName = WishedName.Text;
 
-            var dialogFailed = new MessageDialog("¡Vaya! Parece que ya existe un archivo llamado " + wishedZipName + ". Por favor, cambie el nombre.");
+            var dialogFailed = new ContentDialog();
             var dialogComplete = new MessageDialog("¡Perfecto! Su archivo ya fue comprimido.");
+
+            dialogFailed.Title = "¿Desea sobreescribir?";
+            dialogFailed.Content = "Parece que este archivo ya existe.";
+            dialogFailed.PrimaryButtonText = "Sobreescribir";
+            dialogFailed.CloseButtonText = "Cancelar";
+            dialogFailed.DefaultButton = ContentDialogButton.Primary;
 
             var folderPicker = new FolderPicker();
             folderPicker.SuggestedStartLocation = PickerLocationId.Desktop;
@@ -60,23 +71,46 @@ namespace NewZip
             newFolder = await StorageApplicationPermissions.FutureAccessList.GetFolderAsync("PickedFolderToken");
             zipFolder = await StorageApplicationPermissions.FutureAccessList.GetFolderAsync("ZipFinal");
 
-            if (await Task.Run(() => File.Exists(zipFolder.Path + "/" + wishedZipName + ".zip")))
+            try
             {
-                await dialogFailed.ShowAsync();
-            } else
-            {
-                await Task.Run(() => ZipFile.CreateFromDirectory(newFolder.Path, zipFolder.Path + "/" + wishedZipName + ".zip"));
-                await dialogComplete.ShowAsync();
+                if (await Task.Run(() => File.Exists(zipFolder.Path + "/" + wishedZipName + ".zip")))
+                {
+                    var result = await dialogFailed.ShowAsync();
+                    if (result == ContentDialogResult.Primary)
+                    {
+                        await Task.Run(() => File.Delete(zipFolder.Path + "/" + wishedZipName + ".zip"));
+                        progressItem.Visibility = Visibility.Visible;
+                        await Task.Run(() => ZipFile.CreateFromDirectory(newFolder.Path, zipFolder.Path + "/" + wishedZipName + ".zip"));
+                        progressItem.Visibility = Visibility.Collapsed;
+                        await dialogComplete.ShowAsync();
+                    }
+                }
+                else
+                {
+                    progressItem.Visibility = Visibility.Visible;
+                    await Task.Run(() => ZipFile.CreateFromDirectory(newFolder.Path, zipFolder.Path + "/" + wishedZipName + ".zip"));
+                    progressItem.Visibility = Visibility.Collapsed;
+                    await dialogComplete.ShowAsync();
+                }
             }
-            
+            catch
+            {
+                Debug.WriteLine("Oops...");
+            }           
         }
 
         private async void Button_Click_1(object sender, RoutedEventArgs e)
         {
             string zipName = WishedFolder.Text;
 
-            var dialogFailed = new MessageDialog("¡Vaya! Parece que ya existe una carpeta llamada " + zipName + ". Por favor, cambie el nombre.");
+            var dialogFailed = new ContentDialog();
             var dialogComplete = new MessageDialog("¡Perfecto! Su archivo ya fue descomprimido.");
+
+            dialogFailed.Title = "¿Desea sobreescribir?";
+            dialogFailed.Content = "Parece que esta carpeta ya existe.";
+            dialogFailed.PrimaryButtonText = "Sobreescribir";
+            dialogFailed.CloseButtonText = "Cancelar";
+            dialogFailed.DefaultButton = ContentDialogButton.Primary;
 
             var zipPicker = new FileOpenPicker();
             zipPicker.SuggestedStartLocation = PickerLocationId.Downloads;
@@ -86,26 +120,45 @@ namespace NewZip
             folderPicker.SuggestedStartLocation = PickerLocationId.Downloads;
             folderPicker.FileTypeFilter.Add("*");
 
-            StorageFile fileStorage = await zipPicker.PickSingleFileAsync();
-            StorageFolder folderStorage = await folderPicker.PickSingleFolderAsync();
-
-            StorageApplicationPermissions.FutureAccessList.AddOrReplace("ZipFile", fileStorage);
-            StorageApplicationPermissions.FutureAccessList.AddOrReplace("Folder", folderStorage);
-
-            StorageFile file;
-            StorageFolder folder;
-
-            file = await StorageApplicationPermissions.FutureAccessList.GetFileAsync("ZipFile");
-            folder = await StorageApplicationPermissions.FutureAccessList.GetFolderAsync("Folder");
-
-            if (await Task.Run(() => Directory.Exists(folder.Path + "/" + zipName)))
+            try
             {
-                await dialogFailed.ShowAsync();
-            } else
+                StorageFile fileStorage = await zipPicker.PickSingleFileAsync();
+                StorageFolder folderStorage = await folderPicker.PickSingleFolderAsync();
+
+                StorageApplicationPermissions.FutureAccessList.AddOrReplace("ZipFile", fileStorage);
+                StorageApplicationPermissions.FutureAccessList.AddOrReplace("Folder", folderStorage);
+
+                StorageFile file;
+                StorageFolder folder;
+
+                file = await StorageApplicationPermissions.FutureAccessList.GetFileAsync("ZipFile");
+                folder = await StorageApplicationPermissions.FutureAccessList.GetFolderAsync("Folder");
+
+                if (await Task.Run(() => Directory.Exists(folder.Path + "/" + zipName)))
+                {
+                    var result = await dialogFailed.ShowAsync();
+                    if (result == ContentDialogResult.Primary)
+                    {
+                        await Task.Run(() => Directory.Delete(folder.Path + "/" + zipName, true));
+                        progressItem.Visibility = Visibility.Visible;
+                        await Task.Run(() => ZipFile.ExtractToDirectory(file.Path, folder.Path + "/" + zipName));
+                        progressItem.Visibility = Visibility.Collapsed;
+                        await dialogComplete.ShowAsync();
+                    }
+                }
+                else
+                {
+                    progressItem.Visibility = Visibility.Visible;
+                    await Task.Run(() => ZipFile.ExtractToDirectory(file.Path, folder.Path + "/" + zipName));
+                    progressItem.Visibility = Visibility.Collapsed;
+                    await dialogComplete.ShowAsync();
+                }
+            } catch
             {
-                await Task.Run(() => ZipFile.ExtractToDirectory(file.Path, folder.Path + "/" + zipName));
-                await dialogComplete.ShowAsync();
+                Debug.WriteLine("Oops...");
             }
+
+            
         }
 
         private void ChangeTheme(object sender, RoutedEventArgs e)
@@ -121,6 +174,11 @@ namespace NewZip
                 this.RequestedTheme = ElementTheme.Dark;
                 ThemeImage.Glyph = "\xE708";
             }
+        }
+
+        private void HelpButton_Click(object sender, RoutedEventArgs e)
+        {
+            FlyoutCompress.ShowAt(CenterText);
         }
     }
 }
